424b853bf25ae20a292d57499393f9324a8a7b07
Tanisha0804-t <tanishamumbai2004@gmail.com>
2025-01-16 10:58:00 +0530

    Add files via upload

---
+++ database1.php
@@ -0,0 +1,14 @@
+<?php
+$servername= 'localhost';
+$username = "root";
+$password = "";  
+$database = "optify";
+$conn=mysqli_connect($servername, $username, $password, $database);
+if (!$conn) {
+    die("Connection failed: " .mysqli_connect_error());
+}
+else
+{
+echo "Connected successfully!";
+}
+?>
